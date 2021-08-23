1e8060c84df10aede388986d7ed38a3445c25b79
Jhanpoul23 <60276270+Jhanpoul23@users.noreply.github.com>
2021-08-23 18:43:19 -0500

    avance de practicas
    solo pa que no se me pierda no chingen

--- gestionPracticas/urls.py
+++ gestionPracticas/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path,include
 # from gestionPracticas.views import 
+from gestionPracticas.views import agregarPractica,agregarEmpresa,agregarAsesor,listarPractica
 from django.contrib.auth import views
 urlpatterns = [
   # path('', acceder,name="login"),
   # path('home/',homePage ,name="home"),
+    path('agregarPractica',agregarPractica,name="agregarPractica"),
+    path('agregarEmpresa',agregarEmpresa,name="agregarEmpresa"),
+    path('agregarAsesor',agregarAsesor,name="agregarAsesor"),
+    path('listarPractica',listarPractica,name="listarPractica"),
+
 ]
--- gestionPracticas/views.py
+++ gestionPracticas/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def agregarPractica(request):
+    return render(request,"practica/agregar/estudiante_practica.html")
+
+def agregarEmpresa(request):
+    return render(request,"practica/agregar/empresa_practica.html")
+
+def agregarAsesor(request):
+    return render(request,"practica/agregar/asesor_practica.html")
+
+def listarPractica(request):
+    return render(request,"practica/index.html")
