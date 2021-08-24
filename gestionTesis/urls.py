c38fb7a5d9e59af05f46d6bc101efa7ac3b0ef90
alechuertasfranco <max.alec.huertas@gmail.com>
2021-08-23 21:17:39 -0500

    Maquetado de Tesis

--- gestionTesis/urls.py
+++ gestionTesis/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path,include
-# from gestionPracticas.views import 
+from gestionTesis.views import index_estudiante, index_estudiante_error, index_jurado, index_secretaria
 from django.contrib.auth import views
 urlpatterns = [
-  # path('', acceder,name="login"),
-  # path('home/',homePage ,name="home"),
+  path('tesis_estudiante/', index_estudiante, name="index_estudiante"),
+  path('tesis_estudiante_error/', index_estudiante_error, name="index_estudiante_error"),
+  path('tesis_jurado', index_jurado, name="index_jurado"),
+  path('tesis_secretaria/', index_secretaria, name="index_secretaria"),
 ]
--- gestionTesis/views.py
+++ gestionTesis/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Vistas para estudiantes
+def index_estudiante(request):
+  context={} 
+  return render(request, "index_estudiante.html", context)
+def index_estudiante_error(request):
+  context={'modal': True} 
+  return render(request, "index_estudiante.html", context)
+
+# Vistas para jurado
+def index_jurado(request):
+  context={} 
+  return render(request, "index_jurado.html", context)
+
+# Vistas para secretaria
+def index_secretaria(request):
+  context={} 
+  return render(request, "index_secretaria.html", context)
