c0b6f5ecfec4a26f157e36e17cfb6789c8ce3d7c
alechuertasfranco <max.alec.huertas@gmail.com>
2021-09-02 18:28:53 -0500

    PlanTesis / Asignar_Jurado
    Mejorar el codigo del mantenedor de plan de tesis
    Generar el formulario de Asignar jurado con foms de Django

--- gestionTesis/forms.py
+++ gestionTesis/forms.py
@@ -1,6 +1,6 @@
 from django.forms import *
 from django.forms.widgets import HiddenInput
-from .models import PlanTesis
+from .models import JuradoTesis, PlanTesis
 
 
 class PlanTesisFormCreate(ModelForm):
@@ -13,20 +13,24 @@ class PlanTesisFormCreate(ModelForm):
             'fecha_presentacion': TextInput(attrs={'type': 'date'}),
         }
 
-
 class PlanTesisFormEdit(ModelForm):
     fecha_sustentacion = DateField(
         required=False,
         widget=TextInput(attrs={'type': 'date', 'readonly': 'true'})
     )
-    informe_final = FileField(
-        required=False
-    )
-
+    informe_final = FileField(required=False)
     class Meta:
         model = PlanTesis
         fields = ['asesor', 'fecha_presentacion','fecha_sustentacion', 'proyecto_tesis', 'informe_final']
         widgets = {
             'asesor': Select(attrs={'readonly': 'true'}),
             'fecha_presentacion': TextInput(attrs={'type': 'date', 'readonly': 'true'}),
+        }
+
+class JuradoTesisForm(ModelForm):
+    class Meta:
+        model = JuradoTesis
+        fields = ['docente', 'plan_tesis']
+        labels = {
+            'plan_tesis': 'Alumno',
         }
--- gestionTesis/urls.py
+++ gestionTesis/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path,include
-from gestionTesis.views import index_estudiante, index_estudiante_error, index_jurado, index_secretaria
+from django.urls import path
+from gestionTesis.views import *
 from django.contrib.auth import views
 urlpatterns = [
   path('tesis_estudiante/', index_estudiante, name="index_estudiante"),
   path('tesis_estudiante_error/', index_estudiante_error, name="index_estudiante_error"),
   path('tesis_jurado', index_jurado, name="index_jurado"),
+  path('asignar_jurado', asignar_jurado, name="asignar_jurado"),
   path('tesis_secretaria/', index_secretaria, name="index_secretaria"),
 ]
