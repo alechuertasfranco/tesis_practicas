0c5aba84b0f64fcaac8aeaa7e1c745d59dc8d102
Drako2711 <drako2711xd@gmail.com>
2021-09-06 00:32:40 -0500

    Actualización de campos y migraciones, Gestión Seguridad

--- gestionSeguridad/forms.py
+++ gestionSeguridad/forms.py
@@ -1,15 +1,14 @@
 from django import forms 
 from django.forms import fields
-from .models import Alumno, Docente
+from .models import Alumno, Docente, Facultad, Escuela
 
-class AlumnoForm(forms.ModelForm):
-    class Meta:       
+class AlumnoForm(forms.ModelForm):  
+    class Meta:
         model=Alumno
         #fields='__all__'
         fields=['user','nro_matricula','apellidos','nombres','email','telefono','facultad','escuela','ciclo_academico'] 
-        
+    
 class DocenteForm(forms.ModelForm):
     class Meta:
         model=Docente
-        #fields='__all__'
         fields=['user','apellidos','nombres','titulo','telefono','email']
