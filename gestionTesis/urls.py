6fe9aa20ad869a846bee6150d8d5103fa5d6e76c
alechuertasfranco <max.alec.huertas@gmail.com>
2021-09-10 11:34:21 -0500

    Realizar y subir observaciones de plan de tesis

--- gestionTesis/urls.py
+++ gestionTesis/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
   path('tesis_estudiante_error/', index_estudiante_error, name="index_estudiante_error"),
   path('tesis_docente', index_docente, name="index_docente"),
   path('modal_visar_tesis/<int:plan_id>/', visar_plan_tesis, name="visar_plan_tesis"),
+  path('modal_observar_tesis/<int:observacion_id>/', observar_plan_tesis, name="observar_plan_tesis"),
   path('asignar_jurado', asignar_jurado, name="asignar_jurado"),
   path('tesis_secretaria/', index_secretaria, name="index_secretaria"),
 ]
